0768cff10ae32d65b27e3e8cadb75a6f2b665411
ma2uki <y.matsu880077@gmail.com>
2025-10-14 06:09:25 +0900

    JSchange

--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
 // ページめくり（本をめくるようなイメージ）
+/*
+
+ローディングから画面遷移
+================================================ */
+const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
+const loadingText = document.querySelector('#loading p');
+
+window.addEventListener('load', () => {
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );  
+
+  // ローディング中テキスト
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: .8  //80%
+      },
+      {
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ], 
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
+
 document.addEventListener('DOMContentLoaded', function () {
   const items = document.querySelectorAll('.text-item');
   const prevBtn = document.querySelector('.prev-btn');
@@ -67,61 +123,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
-/*
 
-ローディングから画面遷移
-================================================ */
-const loadingAreaGrey = document.querySelector('#loading');
-const loadingAreaGreen = document.querySelector('#loading-screen');
-const loadingText = document.querySelector('#loading p');
-
-window.addEventListener('load', () => {
-  // ローディング中（グレースクリーン）
-  loadingAreaGrey.animate(
-    {
-      opacity: [1, 0],
-      visibility: 'hidden',
-    },
-    {
-      duration: 2000,
-      delay: 1200,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );
-
-  // ローディング中（薄緑スクリーン）
-  loadingAreaGreen.animate(
-    {
-      translate: ['0 100vh', '0 0', '0 -100vh']
-    },
-    {
-      duration: 2000,
-      delay: 800,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );  
-
-  // ローディング中テキスト
-  loadingText.animate(
-    [
-      {
-        opacity: 1,
-        offset: .8  //80%
-      },
-      {
-        opacity: 0,
-        offset: 1  //100%
-      },
-    ], 
-    {
-      duration: 1200,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );
-});
 
 particlesJS("particles-js-canvas-el", 
   {"particles":
